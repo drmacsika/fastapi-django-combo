6182cfabb6f0360c81d49c8ba6e868607975f9b5
drmacsika <drmacsika@gmail.com>
2021-10-19 15:29:02 +0100

    Resolve issues with api naming.

--- blog/endpoints.py
+++ blog/endpoints.py
@@ -4,17 +4,17 @@ from fastapi import APIRouter
 
 from blog import models, schemas
 
-api_router = APIRouter()
+blog_router = APIRouter()
 
 
-@api_router.post("/posts", response_model=schemas.Post)
+@blog_router.post("/posts", response_model=schemas.Post)
 def create_post(post: schemas.PostCreate):
     post = models.Post.objects.create(**post.dict())
 
     return post
 
 
-@api_router.get("/posts", response_model=List[schemas.Post])
+blog_router.get("/posts", response_model=List[schemas.Post])
 def read_posts():
     posts = list(models.Post.objects.all())
 
