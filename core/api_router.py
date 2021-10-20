6ba92e50dcc546785d5e017418dad1653e2104b8
drmacsika <drmacsika@gmail.com>
2021-10-20 19:13:26 +0100

    Resolve issue with importing APIRouter.

--- core/api_router.py
+++ core/api_router.py
@@ -2,4 +2,4 @@ from blog.endpoints import router as blog_router
 from fastapi import APIRouter
 
 router = APIRouter()
-router.add_router(blog_router, prefix="/blog", tags=["Blog"])
+router.include_router(blog_router, prefix="/blog", tags=["Blog"])
