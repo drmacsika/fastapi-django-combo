788afccbe2e1487c1e863d229bb69173a69023d5
drmacsika <drmacsika@gmail.com>
2021-10-20 21:01:17 +0100

    Add Full models for Blog Post and Category.

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,115 @@
+from datetime import date, datetime
+from pathlib import Path
+from typing import Any, Callable, List, Union
+
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
+from pydantic import AnyUrl
+
+from blog.manager import CategoryManager, PostManager
+
+
+def upload_image_path(
+    instance: Any,
+    filename: str) -> Union[str, Callable, Path]:
+    """
+    Set up the default thumbnail upload path.
+    """
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" %(instance.slug, ext)
+    return "blog_post_thumbnails/%s/%s" %(instance.slug, filename)
+
+
+class Category(models.Model):
+    """
+    Model for blog tags.
+    """
+    title: str = models.CharField(max_length=200)
+    description: str = models.CharField(max_length=200, blank=True, null=True)
+    slug: str = models.SlugField(unique=True, blank=True)
+    active: bool = models.BooleanField(default=False)
+    updated: datetime = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_on: datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects: CategoryManager = CategoryManager()
+
+    class Meta:
+        constraints: List[Any] = [
+            models.UniqueConstraint(
+                fields=['parent', 'slug'],
+                name='unique_blog_category'
+            ),
+        ]
+        verbose_name_plural: str = "categories"
+        
+    def __repr__(self) -> str:
+        return f"<class {self.slug}>"
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    """
+    Model for Blog Posts.
+    """
+    user: Any = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        default=1,
+        on_delete=models.CASCADE,
+        related_name="posts"
     )
+    category: Union[str, list] = models.ManyToManyField(Category)
+    title: str = models.CharField(max_length=250)
+    description: str = models.TextField(null=True, blank=True)
+    content: str = models.TextField()
+    slug: str = models.SlugField(unique=True, blank=True)
+    thumbnail: Union[AnyUrl, str] = models.FileField(
+        upload_to=upload_image_path, 
+        null=True, blank=True, max_length=1000
+    )
+    draft: bool = models.BooleanField(default=False)
+    publish: datetime = models.DateField(auto_now=False, auto_now_add=False)
+    read_time: int = models.IntegerField(default=0)
+    view_count:int = models.PositiveIntegerField(default=0)
+    updated: datetime = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_on: datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+    
+    objects: PostManager = PostManager()
+    
+    class Meta:
+        verbose_name: str = "post"
+        verbose_name_plural: str = "posts"
+        ordering: list = ["-created_on", "-title"]
+
+    def __repr__(self) -> str:
+        return "<Post %r>" % self.title
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+    def get_thumbnail_url(self) -> Union[Path, str, AnyUrl]:
+        timestamp = "%s%s%s%s%s" % (datetime.now().year, datetime.now().day, datetime.now().hour, datetime.now().minute,
+                                    datetime.now().second)
+        if self.thumbnail and hasattr(self.thumbnail, 'url'):
+            return "%s?enc=%s" % (self.thumbnail.url, timestamp)
+        
+
+# def posts_pre_save_receiver(sender, instance, *args, **kwargs):
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+
+#     if instance.content:
+#         html_string = instance.content
+#         calc_read_length = get_read_length(html_string)
+#         instance.read_length = calc_read_length
+
+
+# def category_pre_save_receiver(sender, instance, *args, **kwargs):
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+
+
+# models.signals.pre_save.connect(posts_pre_save_receiver, sender=Post)
+# models.signals.pre_save.connect(category_pre_save_receiver, sender=Category)
