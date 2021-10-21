08efa69beb28e5063242dcb8764a633581c57cb3
drmacsika <drmacsika@gmail.com>
2021-10-21 14:20:53 +0100

    Clean up unused codes and comments.

--- core/urls.py
+++ core/urls.py
@@ -4,16 +4,3 @@ from django.contrib.staticfiles import views
 from django.urls import path
 
 urlpatterns = [path("admin/", admin.site.urls)]
-
-
-# if settings.DEBUG:
-#     from django.urls import re_path
-
-#     urlpatterns += [re_path(r"^static/(?P<path>.*)$", views.serve)]
-
-
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
