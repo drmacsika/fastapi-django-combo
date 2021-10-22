daaec934eb63d6314f83ee58b179c445206e2682
drmacsika <drmacsika@gmail.com>
2021-10-23 00:57:43 +0100

    Add complete endpoints for blog crud

--- blog/endpoints.py
+++ blog/endpoints.py
@@ -1,21 +1,93 @@
-from typing import List
+from typing import Any, List
 
 from fastapi import APIRouter
 
-from blog import models, schemas
+from blog.api_crud import category, post
+from blog.schemas import (AllPostList, CategoryOut, CreateCategory, CreatePost,
+                          PostByCategoryList, SinglePost, UpdateCategory,
+                          UpdatePost)
 
 router = APIRouter()
 
 
-@router.post("/posts/", response_model=schemas.Post)
-def create_post(post: schemas.PostCreate):
-    post = models.Post.objects.create(**post.dict())
+@router.get("/posts/", response_model=List[AllPostList])
+def get_multiple_posts(offset: int = 0, limit: int = 10) -> Any:
+    """
+    Endpoint to get multiple posts based on offset and limit values.
+    """
+    return post.get_multiple(offset=offset, limit=limit)
 
-    return post
+
+@router.post("/posts/", status_code=201, response_model=SinglePost)
+def create_post(request: CreatePost) -> Any:
+    """
+    Endpoint to create single post.
+    """
+    return post.create(obj_in=request)
 
 
-@router.get("/posts/", response_model=List[schemas.Post])
-def read_posts():
-    posts = list(models.Post.objects.all())
+@router.post("/tags/", status_code=201, response_model=CategoryOut)
+def create_category(request: CreateCategory) -> Any:
+    """
+    Endpoint to create single category.
+    """
+    return category.create(obj_in=request)
 
-    return posts
+
+@router.get("/tags/", response_model=List[CategoryOut])
+def get_multiple_categories(offset: int = 0, limit: int = 10) -> Any:
+    """
+    Get multiple categories.
+    """
+    query = category.get_multiple(limit=limit, offset=offset)
+    return list(query)
+
+
+@router.get("/posts/{slug}/", response_model=SinglePost)
+def get_post(slug: str) -> Any:
+    """
+    Get single blog post.
+    """
+    return post.get(slug=slug)
+
+
+@router.get("/tags/{slug}/", response_model=List[PostByCategoryList])
+def get_posts_by_category(slug: str) -> Any:
+    """
+    Get all posts belonging to a particular category
+    """
+    query = post.get_posts_by_category(slug=slug)
+    return list(query)
+
+
+@router.put("/posts/{slug}/", response_model=SinglePost)
+def update_post(slug: str, request: UpdatePost) -> Any:
+    """
+    Update a single blog post.
+    """
+    return post.update(slug=slug, obj_in=request)
+
+
+@router.put("/tags/{slug}/", response_model=CategoryOut)
+def update_category(slug: str, request: UpdateCategory) -> Any:
+    """
+    Update a single blog category.
+    """
+    return category.update(slug=slug, obj_in=request)
+
+
+@router.delete("/posts/{slug}/")
+def delete_post(slug: str) -> Any:
+    """
+    Delete a single blog post.
+    """
+    return post.delete(slug=slug)
+
+
+@router.delete("/tags/{slug}/", response_model=CategoryOut)
+def delete_category(slug: str) -> Any:
+    """
+    Delete a single blog category.
+    """
+    return category.delete(slug=slug)
+
