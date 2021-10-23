306353513a8ddff2d11c71af0d87c4ec3df51568
drmacsika <drmacsika@gmail.com>
2021-10-23 02:42:45 +0100

    Add custom admin.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['email', 'subject', 'active', 'created']
+    list_display_links = ['email']
+    list_filter = ['active', 'created']
+    search_fields = ['subject']
+    list_editable = ['active']
+    list_per_page = 10
+    ordering = ('-id',)
+
+
+admin.site.register(Contact, ContactAdmin)
